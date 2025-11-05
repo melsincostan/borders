aba46e62a4c655815db976ce9c653b3519c9e1d7
Mélusine <melusine.hermier@proton.me>
2025-11-05 09:14:12 +0100

    feat: make the sort of the leaderbord put country with the most amount of crossings on top in case of ties

--- db/country/read.go
+++ db/country/read.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	leaderboardQuery = "SELECT countries.*, CAST(COUNT(DISTINCT(tbc.id)) AS REAL) / COUNT(DISTINCT(tc.id)) AS ratio, COUNT(DISTINCT(tc.id)) AS crossings, COUNT(DISTINCT(tbc.id)) AS checks, COUNT(DISTINCT(tpc.id)) AS papers_checks FROM countries LEFT JOIN crossings AS tc ON tc.country_id = countries.id LEFT JOIN crossings AS tbc ON tbc.country_id = countries.id AND tbc.border_check = 1 LEFT JOIN crossings AS tpc ON tpc.country_id = countries.id AND tpc.papers_check = 1 GROUP BY countries.id HAVING crossings > 0 ORDER BY ratio DESC"
+	leaderboardQuery = "SELECT countries.*, CAST(COUNT(DISTINCT(tbc.id)) AS REAL) / COUNT(DISTINCT(tc.id)) AS ratio, COUNT(DISTINCT(tc.id)) AS crossings, COUNT(DISTINCT(tbc.id)) AS checks, COUNT(DISTINCT(tpc.id)) AS papers_checks FROM countries LEFT JOIN crossings AS tc ON tc.country_id = countries.id LEFT JOIN crossings AS tbc ON tbc.country_id = countries.id AND tbc.border_check = 1 LEFT JOIN crossings AS tpc ON tpc.country_id = countries.id AND tpc.papers_check = 1 GROUP BY countries.id HAVING crossings > 0 ORDER BY ratio DESC, crossings DESC"
 )
 
 func Read(db *gorm.DB) (res []models.Country, err error) {
--- db/transport/read.go
+++ db/transport/read.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	leaderboardQuery = "SELECT transports.*, CAST(COUNT(DISTINCT(tbc.id)) AS REAL) / COUNT(DISTINCT(tc.id)) AS ratio, COUNT(DISTINCT(tc.id)) AS crossings, COUNT(DISTINCT(tbc.id)) AS checks, COUNT(DISTINCT(tpc.id)) AS papers_checks FROM transports LEFT JOIN crossings AS tc ON tc.transport_id = transports.id LEFT JOIN crossings AS tbc ON tbc.transport_id = transports.id AND tbc.border_check = 1 LEFT JOIN crossings AS tpc ON tpc.transport_id = transports.id AND tpc.papers_check = 1 GROUP BY transports.id HAVING crossings > 0 ORDER BY ratio DESC"
+	leaderboardQuery = "SELECT transports.*, CAST(COUNT(DISTINCT(tbc.id)) AS REAL) / COUNT(DISTINCT(tc.id)) AS ratio, COUNT(DISTINCT(tc.id)) AS crossings, COUNT(DISTINCT(tbc.id)) AS checks, COUNT(DISTINCT(tpc.id)) AS papers_checks FROM transports LEFT JOIN crossings AS tc ON tc.transport_id = transports.id LEFT JOIN crossings AS tbc ON tbc.transport_id = transports.id AND tbc.border_check = 1 LEFT JOIN crossings AS tpc ON tpc.transport_id = transports.id AND tpc.papers_check = 1 GROUP BY transports.id HAVING crossings > 0 ORDER BY ratio DESC, crossings DESC"
 )
 
 func Read(db *gorm.DB) (res []models.Transport, err error) {
